c910be38f05d4bb84a4d7f20a31882dba6f341d1
saadullahkhan51 <saadullahkhan5151@gmail.com>
2023-01-17 16:50:56 +0500

    efficient and clean

--- content.js
+++ content.js
@@ -1,25 +1,16 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'convert_code_naming_conventions') {
-
-    const highlightedWords = [];
-    var wordDict = {};
     const codeElements = document.querySelectorAll('code');
-
-    codeElements.forEach(function (codeElement) {
-      const codeText = codeElement.textContent;
-      const words = codeText.split(' ');
-      highlightedWords.push(...words);
-    });
-
-    let lang = request.language;
-    highlightedWords.forEach(function (word) {
-      if (lang == 'python') {
-        wordDict[word] = word.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase();
-      } else {
-        wordDict[word] = word.replace(/(_)(.)/g, (match, _, character) => character.toUpperCase());
-      }
-    });
-    console.log(wordDict);
+    const codeTexts = [...codeElements].map(codeElement => codeElement.textContent);
+    const highlightedWords = [...new Set(codeTexts.join(" ").split(" "))];
+    const lang = request.language;
+    const wordDict = Object.fromEntries(highlightedWords.filter(word => {
+      let replacedWord = lang === 'python' ? word.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase() : word.replace(/(_)(.)/g, (match, _, character) => character.toUpperCase());
+      return word !== replacedWord;
+    }).map(word => {
+      let replacedWord = lang === 'python' ? word.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase() : word.replace(/(_)(.)/g, (match, _, character) => character.toUpperCase());
+      return [word, replacedWord];
+    }));    
     chrome.storage.local.set({ wordDict: wordDict }, function () {
       console.log('wordDict is stored');
     });
